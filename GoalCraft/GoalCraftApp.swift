1d30cf95d99db8cb39066706973f4855ec0bfb1b
Bruno Januário <januario@MacBook-Air-de-Bruno.local>
2023-12-29 17:56:56 +0000

    initial commit

---
+++ GoalCraft/GoalCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalCraftApp.swift
+//  GoalCraft
+//
+//  Created by Bruno Januário on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GoalCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
